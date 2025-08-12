bc367ff698515673f6b872ad278e2c928e9de36c
Caio Harrys <blackiaratanmusic@gmail.com>
2025-08-12 00:24:27 -0300

    SavePoint
    Firebase auth error˜bug

--- Doodle/firebase.ts
+++ Doodle/firebase.ts
@@ -1,8 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // Importa apenas o getAuth principal por enquanto
-import { getAuth } from 'firebase/auth';
-
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyCh-YIwVX2tsHbb6RrVglATCNaS0lmj8Qg",
   authDomain: "doodle-df91a.firebaseapp.com",
@@ -10,14 +9,13 @@ const firebaseConfig = {
   storageBucket: "doodle-df91a.firebasestorage.app",
   messagingSenderId: "189303125266",
   appId: "1:189303125266:web:3c19140961d18f8ece6fcb",
-  measurementId: "G-XRME18H0QN"
+  measurementId: "G-XRME18H0QN",
 };
-
 // Inicializa o Firebase App
 const app = initializeApp(firebaseConfig);
 
 // Inicializa o Auth da forma mais simples.
-// Isso vai gerar um AVISO no console, mas não deve quebrar o app.
+// Isso vai gerar um AVISO amarelo no console, mas não deve quebrar o app.
 const auth = getAuth(app);
 
 // Exporta os serviços que vamos usar
